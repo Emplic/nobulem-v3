39276e7fcfe433cd7f0440fe4e20aa1782df1321
Emplic <89635087+Emplic@users.noreply.github.com>
2026-07-18 15:55:58 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,108 +8,126 @@ local Games = {
         PlaceIds        = { 87018676608089 },
         GameName        = "Pistol Arena",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "69589197133432135098",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 85207102870777, 90568084448279 },
         GameName        = "One Tap",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "30705437441887937520",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 99342262733194 },
         GameName        = "Randomizer Redux",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "29407075271633530921",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 5307215810 },
         GameName        = "Randomizer Legacy",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "38307661723468074678",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 119259569670784, 122446657157717, 126042865144779, 119259569670784, 96216501849190, 124955530864032, 114188007571146, 101571206862372, 90165746516953, 74424488747487, 119661268047775, 111189101942839, 113390337779988, 115517196855730, 109094919875208, 102220551718323, 90625015569871, 112261221918322, 92726474449929, 125154235269776 },
         GameName        = "Sniper Arena",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "72877165672068346715",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 4639625707 },
         GameName        = "War Tycoon",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "49408082951445804304",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 6847090259 },
         GameName        = "Bulked Up",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "91444182629983667670",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 136801880565837 },
         GameName        = "Flick",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "69056539903019355219",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 98927955463992, 114204398207377 },
         GameName        = "Survive Zombie Arena",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "51125809171480663668",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 134558434771720, 130736793665498, 91654345256890, 92216849541624, 95439417741900, 108281974227624, 124085311761907 },
         GameName        = "Arcade Basketball",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "50947502008710067489",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 13997264379, 13997018456, 13997531040 },
         GameName        = "Operations Siege",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "23032188996693215788",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 100040622766961 },
         GameName        = "Hypershot",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "10004062276696100000",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 94590879393563, 98784084213911, 96076204236629, 82097489006022 },
         GameName        = "Weird Gun Game",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "35186030186274132053",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 15555246249, 107316497864340, 76759187707484, 74400058410183, 121395544767340, 116147279609754, 70816182080869, 117482669510587 },
         GameName        = "Dogs of War",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "79176565552022168009",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 3678761576 },
         GameName        = "Entrenched",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "10283665115579302067",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 95721658376580 },
         GameName        = "Multicrew Tank Combat",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "00000000000000000000",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 6804602922 },
         GameName        = "1B Boxing",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "00000000000000000000",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
     {
         PlaceIds        = { 17625359962, 129604661913557, 133215910299950, 117398147513099, 18126510175, 71874690745115 },
         GameName        = "Rivals",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "13863774256428152037",
         GetKeyUrl       = "https://nobulem.wtf/key",
         Discontinued    = true,
     },
@@ -117,6 +135,7 @@ local Games = {
         PlaceIds        = { 98800969324557 },
         GameName        = "Storage Hunters: Open World",
         SaveFile        = "nobulem_key.txt",
+        LuaProtScriptId = "09574318442573634463",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
 }
@@ -166,13 +185,13 @@ end
 
 local cfg = ResolveGame(game.PlaceId)
 if not cfg then
-    -- Syscure is the routing source of truth. Unknown places are passed to its
-    -- loader, which can use a newly-added mapping or its configured fallback.
-    cfg = {
-        GameName  = game.Name or ("Place " .. tostring(game.PlaceId)),
-        SaveFile  = "nobulem_key.txt",
-        GetKeyUrl = "https://nobulem.wtf/key",
-    }
+    Notify(
+        "Unsupported Game",
+        ("nobulem.wtf does not currently support this game (PlaceId: %d)."):format(game.PlaceId),
+        10
+    )
+    getgenv().nobulem_loader_started = nil
+    return
 end
 
 if cfg.Discontinued then
@@ -186,12 +205,11 @@ if cfg.Discontinued then
 end
 
 getgenv().NobulemLoaderConfig = {
-    PlaceId          = game.PlaceId,
-    GameName         = cfg.GameName,
-    SaveFile         = cfg.SaveFile,
-    GetKeyUrl        = cfg.GetKeyUrl,
-    SyscureLoaderId  = "b210d4a5705efb0abe7c5590",
-    SyscureLoaderUrl = "https://auth.syscure.vip/loader/b210d4a5705efb0abe7c5590.lua",
+    PlaceId         = game.PlaceId,
+    GameName        = cfg.GameName,
+    SaveFile        = cfg.SaveFile,
+    LuaProtScriptId = cfg.LuaProtScriptId,
+    GetKeyUrl       = cfg.GetKeyUrl,
 }
 
 local function InviteDiscord()
@@ -222,7 +240,10 @@ end
 task.spawn(InviteDiscord)
 
 local ok, err = pcall(function()
-    loadstring(game:HttpGet(KEYSYSTEM_URL))()
+    local source = game:HttpGet(KEYSYSTEM_URL)
+    local chunk, compileErr = loadstring(source)
+    if not chunk then error("compile: " .. tostring(compileErr)) end
+    chunk(getgenv().NobulemLoaderConfig)
 end)
 
 if not ok then
