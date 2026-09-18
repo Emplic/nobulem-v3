9226911ec949603f2495d27848d60a8aad89d37a
John Smith <89635087+Emplic@users.noreply.github.com>
2026-09-19 01:45:01 +0300

    Update LuaProtScriptId in loader.lua

--- loader.lua
+++ loader.lua
@@ -192,7 +192,7 @@ local Games = {
         PlaceIds        = { 13883059853, 11468075017, 5956785391, 9627847912},
         GameName        = "Project Slayers 2",
         SaveFile        = "nobulem_key.txt",
-        LuaProtScriptId = "19288386138978870282",
+        LuaProtScriptId = "63969555156456004056",
         GetKeyUrl       = "https://nobulem.wtf/key",
     },
 }
